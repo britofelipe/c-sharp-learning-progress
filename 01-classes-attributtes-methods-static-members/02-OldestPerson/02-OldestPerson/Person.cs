53b37fba76598b058faf7bdc4b197f67b74bb31d
britofelipe <arrudabritofelipe@gmail.com>
2022-12-29 10:30:12 -0300

    Exercise 1

---
+++ 01-classes-attributtes-methods-static-members/02-OldestPerson/02-OldestPerson/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_OldestPerson
+{
+    internal class Person
+    {
+        public String name;
+        public int age;
+    }
+}
---
+++ 01-classes-attributtes-methods-static-members/02-OldestPerson/02-OldestPerson/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace _02_OldestPerson
+{
+    class OldestPerson
+    {
+        static void Main(string[] args)
+        {
+            Person person1, person2;
+            person1 = new Person();
+            person2 = new Person();
+
+            // First Person
+            Console.WriteLine("Enter the first person name: ");
+            person1.name = Console.ReadLine();
+
+            Console.WriteLine("Enter the first person age: ");
+            person1.age = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Second Person
+            Console.WriteLine("Enter the first person name: ");
+            person2.name = Console.ReadLine();
+
+            Console.WriteLine("Enter the first person age: ");
+            person2.age = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Person 1: " + person1.name + ", " + person1.age.ToString() + " years old");
+            Console.WriteLine("Person 2: " + person2.name + ", " + person2.age.ToString() + " years old");
+
+            if(person1.age > person2.age)
+            {
+                Console.WriteLine("Oldest Person: " + person1.name);
+            } 
+            else
+            {
+                Console.WriteLine("Oldest Person: " + person2.name);
+            }
+            
+
+        }
+    }
+}
