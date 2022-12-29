3b90b70da180897980ccf401bd490400a3f2df3e
britofelipe <arrudabritofelipe@gmail.com>
2022-12-29 10:48:13 -0300

    Exercise 2

--- 01-classes-attributtes-methods-static-members/03-MediumSalary/03-MediumSalary/Program.cs
+++ 01-classes-attributtes-methods-static-members/03-MediumSalary/03-MediumSalary/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace _03_MediumSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker1, worker2;
+            worker1 = new Worker();
+            worker2 = new Worker();
+
+            // First Worker
+            Console.WriteLine("Enter the first worker name: ");
+            worker1.name = Console.ReadLine();
+
+            Console.WriteLine("Enter the first worker salary: ");
+            worker1.salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Second Worker
+            Console.WriteLine("Enter the first worker name: ");
+            worker2.name = Console.ReadLine();
+
+            Console.WriteLine("Enter the first worker salary: ");
+            worker2.salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Worker 1: " + worker1.name + ", " + worker1.salary.ToString("F2") + "$ (monthly)");
+            Console.WriteLine("Worker 2: " + worker2.name + ", " + worker2.salary.ToString("F2") + "$ (monthly)");
+
+            Console.WriteLine("Mean salary: " + ((worker1.salary + worker2.salary)/2) + "$");
+        }
+    }
+}
---
+++ 01-classes-attributtes-methods-static-members/03-MediumSalary/03-MediumSalary/Worker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MediumSalary
+{
+    class Worker
+    {
+        public String name;
+        public double salary;
+    }
+}
