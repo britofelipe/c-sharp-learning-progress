d00d50ad1aef914e9487014514aabb671f536e9e
britofelipe <arrudabritofelipe@gmail.com>
2023-01-23 10:15:05 -0300

    Student

---
+++ 01-classes-attributtes-methods-static-members/06-Student/06-Student/Entities/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Student.Entities
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public double Grade1, Grade2, Grade3;
+        public Student() { }
+
+        public double FinalGrade()
+        {
+            return Grade1 + Grade2 + Grade3;
+        }
+
+        public string FinalStatus()
+        {
+            if (FinalGrade() >= 60)
+            {
+                return "APPROVED";
+            }
+            else
+            {
+                return "REPROVED\n" +
+                    "You were " + (60 - FinalGrade()) + " points short of being approved";
+            }
+        }
+    }
+}
---
+++ 01-classes-attributtes-methods-static-members/06-Student/06-Student/Program.cs
@@ -0,0 +1,26 @@
+﻿using _06_Student.Entities;
+using System.Globalization;
+
+namespace _06_Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            Console.Write("Student's name: ");
+            student.Name = Console.ReadLine();
+
+            Console.WriteLine("Write student's three grades: ");
+            student.Grade1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.Grade2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.Grade3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("FINAL GRADE: " + student.FinalGrade().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(student.FinalStatus());
+
+
+        }
+    }
+}
