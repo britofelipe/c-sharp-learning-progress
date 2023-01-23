858fb22df1cea58c9cc5e873e77f959e3a01f62c
britofelipe <arrudabritofelipe@gmail.com>
2023-01-23 10:58:17 -0300

    Dollar

---
+++ 01-classes-attributtes-methods-static-members/Dollar/Dollar/Entities/Rate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dollar.Entities
+{
+    internal class Rate
+    {
+        public static double DollarRealRate;
+
+        public static double TotalValue(double amount)
+        {
+            return 1.06 * amount * DollarRealRate;
+        }
+    }
+}
---
+++ 01-classes-attributtes-methods-static-members/Dollar/Dollar/Program.cs
@@ -0,0 +1,18 @@
+﻿using Dollar.Entities;
+using System.Globalization;
+
+namespace Dollar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the Dollar cotation? ");
+            Rate.DollarRealRate = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("How much dollar do you want to buy? ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Value to be payed (in Real): ");
+            Console.WriteLine(Rate.TotalValue(amount).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
