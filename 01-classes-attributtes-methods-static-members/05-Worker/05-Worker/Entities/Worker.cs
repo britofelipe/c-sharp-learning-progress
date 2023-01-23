9bfb20e85602ed6192b6ec74d01facae6f682d8b
britofelipe <arrudabritofelipe@gmail.com>
2023-01-23 09:13:55 -0300

    Worker

---
+++ 01-classes-attributtes-methods-static-members/05-Worker/05-Worker/Entities/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Worker.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public double GrossSalary { get; set; }
+        public double Taxes { get; set; }
+
+        public double NetSalary()
+        {
+            return GrossSalary - Taxes;
+        }
+
+        public void IncrementSalary(double percentage)
+        {
+            GrossSalary = GrossSalary*(1+(percentage/100));
+        }
+
+    }
+}
---
+++ 01-classes-attributtes-methods-static-members/05-Worker/05-Worker/Program.cs
@@ -0,0 +1,26 @@
+﻿using _05_Worker.Entities;
+using System.Globalization;
+
+namespace _05_Worker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker= new Worker();
+
+            Console.Write("Name: ");
+            worker.Name = Console.ReadLine();
+            Console.Write("Gross Salary: ");
+            worker.GrossSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Taxes: ");
+            worker.Taxes = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Worker: " + worker.Name + ", $ " + worker.NetSalary());
+            Console.Write("Write the percentage in which you want to increment the worker's salary: ");
+            double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            worker.IncrementSalary(percentage);
+            Console.WriteLine("Worker: " + worker.Name + ", $ " + worker.NetSalary());
+        }
+    }
+}
