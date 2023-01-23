fa050f06a87131fd2bffe6071463514208f9d74c
britofelipe <arrudabritofelipe@gmail.com>
2023-01-23 08:58:18 -0300

    Rectangle

---
+++ 01-classes-attributtes-methods-static-members/04-Rectangle/04-Rectangle/Entities/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Rectangle.Entities
+{
+    internal class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public double Area ()
+        {
+            return (Width * Height);
+        }
+
+        public double Perimeter ()
+        {
+            return 2*(Width + Height);
+        }
+
+        public double Diagonal ()
+        {
+            return Math.Sqrt(Width*Width + Height*Height);
+        }
+    }
+}
---
+++ 01-classes-attributtes-methods-static-members/04-Rectangle/04-Rectangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using _04_Rectangle.Entities;
+using System.Globalization;
+
+namespace _04_Rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+
+            Console.WriteLine("Enter the width and the height of the rectangle:");
+            r.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA: " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETER: " + r.Perimeter().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL: " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
