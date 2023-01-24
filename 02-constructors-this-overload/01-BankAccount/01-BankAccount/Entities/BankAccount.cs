96529f52695ef2213fcd29c501861a530bd64962
britofelipe <arrudabritofelipe@gmail.com>
2023-01-24 18:00:50 -0300

    BankAccount

---
+++ 02-constructors-this-overload/01-BankAccount/01-BankAccount/Entities/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_BankAccount.Entities
+{
+    internal class BankAccount
+    {
+        public int Id { get; set; }
+        public string Name { get; protected set; }
+        public double Balance { get; protected set; }
+
+        public BankAccount(int id, string name, double initialDeposit)
+        {
+            Id = id;
+            Name = name;
+            Balance = initialDeposit;
+        }
+
+        public BankAccount(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Balance = 0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+            return;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+            Console.WriteLine("Your withdraw has costed you $5");
+            Balance -= 5;
+            return;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + Id + ", Owner: " + Name + ", Balance: $" + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 02-constructors-this-overload/01-BankAccount/01-BankAccount/Program.cs
@@ -0,0 +1,53 @@
+﻿using _01_BankAccount.Entities;
+using System.Globalization;
+
+namespace _01_BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account;
+            Console.Write("Please enter user id: ");
+            int id = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Please enter user name: ");
+            string name = Console.ReadLine();
+            Console.Write("Do you want to make a initial deposit? (y/n): ");
+            char option = char.Parse(Console.ReadLine());
+
+            if (option == 'Y' || option == 'y')
+            {
+                Console.Write("Please write your initial deposit: ");
+                double initialDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new BankAccount(id, name, initialDeposit);     
+            }
+            else
+            {
+                account = new BankAccount(id, name);
+            }
+
+            // Data
+            Console.WriteLine();
+            Console.WriteLine("Account data:");
+            Console.WriteLine(account);
+
+            // Deposit
+            Console.WriteLine();
+            Console.Write("Enter the amount for deposit: ");
+            double depositAmount = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+            account.Deposit(depositAmount);
+
+            Console.WriteLine("Updated account data:");
+            Console.WriteLine(account);
+
+            // Withdraw
+            Console.WriteLine();
+            Console.Write("Enter the amount to withdraw: ");
+            double withdrawAmount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Withdraw(withdrawAmount);
+
+            Console.WriteLine("Updated account data:");
+            Console.WriteLine(account);
+        }
+    }
+}
