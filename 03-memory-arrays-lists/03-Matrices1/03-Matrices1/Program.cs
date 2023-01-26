fc98c922c5c3631d3c79cdfb65c6719c10a4de2c
britofelipe <arrudabritofelipe@gmail.com>
2023-01-26 10:03:00 -0300

    Matrices 1

---
+++ 03-memory-arrays-lists/03-Matrices1/03-Matrices1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _03_Matrices1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the matrix dimension: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            Console.WriteLine("Write values of each row separated by a space (' '):");
+            // Input
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            // Output
+            Console.Write("Main diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(matrix[i, i] + " ");
+            }
+            int numberOfNegativeNumbers = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if (matrix[i,j] < 0)
+                    {
+                        numberOfNegativeNumbers++;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Negative numbers: " + numberOfNegativeNumbers);
+        }
+    }
+}
