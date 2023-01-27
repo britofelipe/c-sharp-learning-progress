7f0abff2fb37367ace88f60433203cebe931b89c
britofelipe <arrudabritofelipe@gmail.com>
2023-01-27 12:01:38 -0300

    Worker contracts initial commit

---
+++ 04-enums-composition/02-WorkerContracts/WorkerContracts/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkerContracts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
