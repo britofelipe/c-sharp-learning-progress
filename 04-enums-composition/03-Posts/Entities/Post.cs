228863fc004f8e39bed6131fbf662303b32f7764
britofelipe <arrudabritofelipe@gmail.com>
2023-02-01 08:58:09 -0300

    String Builder

---
+++ 04-enums-composition/03-Posts/Entities/Post.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+namespace _03_Posts.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post() { }
+
+        public Post(DateTime moment, string title, string content)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = 0;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public void LikePost()
+        {
+            Likes++;
+        }
+
+        public void RemoveLikeFromPost()
+        {
+            Likes--;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes + " Likes - ");
+            sb.AppendLine(Moment.ToString("dd/mm/yyyy hh:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+            foreach(Comment comment in Comments)
+            {
+                sb.AppendLine(comment.Text); 
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ 04-enums-composition/03-Posts/Program.cs
@@ -0,0 +1,48 @@
+﻿using _03_Posts.Entities;
+
+namespace _03_Posts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zeland",
+                "I'm goint to visit this wonderful country"
+                );
+
+            for(int i  = 0; i < 12; i++)
+            {
+                post1.LikePost();
+            }
+
+            Comment comment1 = new Comment("Have a nice trip");
+            Comment comment2 = new Comment("Wow that's awesome!");
+
+            post1.AddComment(comment1);
+            post1.AddComment(comment2);
+
+
+            Post post2 = new Post(
+               DateTime.Parse("28/07/2018 23:14:19"),
+               "Good night guys",
+               "See you tomorrow"
+               );
+
+            for (int i = 0; i < 5; i++)
+            {
+                post2.LikePost();
+            }
+
+            Comment comment3 = new Comment("Good night");
+            Comment comment4 = new Comment("May the Force be with you");
+
+            post2.AddComment(comment3);
+            post2.AddComment(comment4);
+
+            Console.WriteLine(post1);
+            Console.WriteLine(post2);
+        }
+    }
+}
